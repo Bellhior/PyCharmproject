294e485e667db35039256c4535a34d3406ece40e
Bellhior <cliff_burton85@mail.ru>
2023-05-26 01:53:26 +0400

    main 2nd commit

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-from funcs import sum_
+from funcs import sum_, sub_
 
 number_1 = int(input('Enter 1st number: '))
 number_2 = int(input('Enter 2nd number: '))
 
 print(f'Сумма {number_1} и {number_2} = {sum_(number_1, number_2)}')
+
+number_3 = int(input('Enter reduced: '))
+number_4 = int(input('Enter deductible: '))
+
+print(f'Разность {number_3} и {number_4} = {sub_(number_3, number_4)}')
