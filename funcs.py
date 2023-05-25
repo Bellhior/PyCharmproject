eb9739ceaea29d7278363fe3453a68c768ab3fd5
Bellhior <cliff_burton85@mail.ru>
2023-05-26 01:15:38 +0400

    1st commit

---
+++ funcs.py
@@ -0,0 +1,8 @@
+def sum_(a, b):
+    return a + b
+
+
+number_1 = int(input('Enter 1st number: '))
+number_2 = int(input('Enter 2nd number: '))
+
+print(f'Сумма {number_1} и {number_2} = {sum_(number_1, number_2)}')
