db964b78fdb40c2f721c20ac4dfc1922ab5a00f1
Bellhior <cliff_burton85@mail.ru>
2023-05-26 01:45:48 +0400

    2nd commit

--- funcs.py
+++ funcs.py
@@ -2,19 +2,8 @@ def sum_(a, b):
     return a + b
 
 
-number_1 = int(input('Enter 1st number: '))
-number_2 = int(input('Enter 2nd number: '))
-
-print(f'Сумма {number_1} и {number_2} = {sum_(number_1, number_2)}')
+"""Перемудрил. Функции идут отдельно тут"""
 
 
 def sub_(c, d):
     return c - d
-
-
-"""Знаю, что функции пишутся вначале, я для наглядности скриншота так сделал"""
-
-number_3 = int(input('Enter reduced: '))
-number_4 = int(input('Enter deductible: '))
-
-print(f'Разность {number_3} и {number_4} = {sub_(number_3, number_4)}')
